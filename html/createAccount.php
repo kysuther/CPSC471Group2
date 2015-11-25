86e18a0c689a58bfb547dc28c540047fe6d4836d
Kyle Sutherland <kysuther@live.ca>
2015-11-25 17:14:12 -0500

    changed password entries from 'text' to 'password'

--- html/createAccount.php
+++ html/createAccount.php
@@ -21,7 +21,7 @@
 
 	<p>
     	<label for="password">Password:</label>
-        <input type="text" name="password" id="password">
+        <input type="password" name="password" id="password">
     </p>
 
 
