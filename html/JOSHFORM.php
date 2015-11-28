fdd6659539113f176e4d200e07dc9749ebe71b7c
Andrew <andrew.mh.dong@gmail.com>
2015-11-28 16:26:02 -0700

    Added new form and fixed database so you can't enter duplicate values

--- html/JOSHFORM.php
+++ html/JOSHFORM.php
@@ -1,47 +1,73 @@
 <?php
-	include("header.php");
-	echo '<br />';
-	echo '<br />';
-	echo '<br />';
-
+	echo "<h1>Hello World</h1>";
+	$link = mysqli_connect("localhost", "root", "","CPSC471_musicdatabase");
+	
 	if($link === false)
 	{
 		die("ERROR: could not connect. " . mysqli_connect_error());
 	}
-
-
+	
+		
 	$Artist_name = mysqli_real_escape_string($link, $_POST['artistName']);
 	$ArtistWebpage = mysqli_real_escape_string($link, $_POST['webpage']);
 	$song_name = mysqli_real_escape_string($link, $_POST['songName']);
 	$song_length = mysqli_real_escape_string($link,$_POST['songlength']);
 	$genre = mysqli_real_escape_string($link,$_POST['Genre']);
+	$Album_name = mysqli_real_escape_string($link,$_POST['albumName']);
+	$albumYear =  mysqli_real_escape_string($link,$_POST['albumYear']);
+	$alNumOfTracks = mysqli_real_escape_string($link,$_POST['albumNumOfTracks']);
+	
+	
+	// Checking if Artist name exists - We do not want duplicates for artist names. If 
+	// The artist name doesn't exist then we insert it.
+	// Only need to insert into the SONGs Table
+	
 
-	
-	if($sql = "SELECT * FROM `artists` WHERE `name` = '$Artist_name'";)
-	{
-		// If the artist name is the same then we want to find the matching artistID in Song
-	}
-	
-	
-	
-	$sql = "INSERT IGNORE INTO artists (name,webpage)
-	VALUES('$Artist_name', '$ArtistWebpage')";
+
+	/*
+		 This will insert the values into the Artists Table
+	*/
+	$sql = "INSERT INTO artists (name, webpage)
+				SELECT * FROM (SELECT '$Artist_name', '$ArtistWebpage') AS tmp
+				WHERE NOT EXISTS (
+					SELECT name FROM artists WHERE name = '$Artist_name'
+				) LIMIT 1";
 	if(mysqli_query($link, $sql))
 	{
 		//echo "Insert was sucessful!";
 	}
-	else
-	{
-		echo "ERROR: Could not execute $sql." . mysqli_error($link);
-	}
+	
 
-	$sql = "INSERT INTO song (name,genre,Length)
-	VALUES('$song_name', '$genre', '$song_length')";
+	/*
+		inserting values into song table
+	*/
+	$sql = "INSERT INTO song (name, length, genre)
+				SELECT * FROM (SELECT '$song_name', '$song_length','$genre') AS tmp
+				WHERE NOT EXISTS (
+					SELECT name FROM song WHERE name = '$song_name'
+				) LIMIT 1";
 	if(mysqli_query($link, $sql))
 	{
-		//echo "DOuble CUCEESS";
+		//echo "Insert was sucessful!";
 	}
+	
+	/*
+		Inserting values into album table 
+	*/
+	$sql = "INSERT INTO album (name, year, numTracks)
+				SELECT * FROM (SELECT '$Album_name', '$albumYear','$alNumOfTracks') AS tmp
+				WHERE NOT EXISTS (
+					SELECT name FROM album WHERE name = '$Album_name'
+				) LIMIT 1";
+	if(mysqli_query($link, $sql))
+	{
+		//echo "Insert was sucessful!";
+	}
+	
+	
 
 
-	include("footer.php");
+	
+	
+	mysqli_close($link);
 ?>
--- html/artistInsertForm.php
+++ html/artistInsertForm.php
@@ -1,46 +1,65 @@
-<?php include("header.php") ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <title>Add Records Form</title>
 </head>
-<br />
-<br />
-<br />
 <body>
 <form action="JOSHFORM.php" method="post">
 	<p>
     	<label for="artistName">Artist Name:</label>
         <input type="text" name="artistName" id="artistName">
     </p>
-    <p>
+   
+   <p>
     	<label for="webpage">ArtistWebpage:</label>
         <input type="text" name="webpage" id="webpage">
     </p>
 
+	
+	
 	<p>
     	<label for="songName">Song Name:</label>
         <input type="text" name="songName" id="songName">
     </p>
+	
+
 
 	<p>
     	<label for="songlength">Song Length:</label>
         <input type="text" name="songlength" id="songlength">
     </p>
-
+	
 	<p>
     	<label for="Genre">Genre:</label>
         <input type="text" name="Genre" id="Genre">
     </p>
-
+	
 	<p>
     	<label for="albumName">Album Name:</label>
         <input type="text" name="albumName" id="albumName">
     </p>
-
-
-
-
-    <input type="submit" value="REGISTER">
+	
+	<p>
+    	<label for="albumYear">Album Year:</label>
+        <input type="text" name="albumYear" id="albumYear">
+    </p>
+	
+	<p>
+    	<label for="albumNumOfTracks">Album NoOfTracks</label>
+        <input type="text" name="albumNumOfTracks" id="albumNumOfTracks">
+    </p>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    <input type="submit" value="SUBMIT">
 </form>
 </body>
-<?php include("footer.php") ?>
+</html>
