c6d849dc335394f48014eff3906722530c4fbaed
Charlie Cheung <charliecheung@users.noreply.github.com>
2015-12-01 15:25:19 -0700

    added css

--- html/register.php
+++ html/register.php
@@ -1,11 +1,46 @@
 <!--REGISTER PAGE-->
 
 <?php include 'headerphp.php' ?>
-
-
+<style type="text/css">
+#topimage {
+    position:absolute;
+    top:20px;
+}
+#topimage img {
+    height:100%;
+    width:100%;
+}
+form {
+    position:fixed;
+    z-index:9999999;
+    top:250px;
+    left:100px;
+    width: 350px;
+    font-style:none;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; 
+    color:#fff;
+    letter-spacing:1px;
+    font-size:20px;
+    background-color:#2a2a2a;
+    padding:10px 10px 10px 10px;
+    border:1px solid #2a2a2a;
+    border-radius: 20px;
+}
+button {
+    width:120px;
+    height:45px;
+    margin-left:32%;
+    font-style:none;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; 
+    color:#fff;
+    letter-spacing:1px;
+    font-size:20px;
+    background-color:#525252;
+}
+button:hover {
+    background-color:#63d297;
+}
 </style>
-</head>
-<body>
 
 <div id="topimage">
     <img src="http://i68.tinypic.com/244o8sk.png">
