570eea16b54812376f8dc2161d07751b88736d4b
Charlie Cheung <charliecheung@users.noreply.github.com>
2015-12-01 14:50:24 -0700

    imgsection and imgcontainer added to body

--- html/index.php
+++ html/index.php
@@ -1,7 +1,11 @@
 <?php include("header.php") ?>
 
+<div id="imgcontainer">
+<div class="imgsection">
 <div id="topimage">
     <img src="http://i68.tinypic.com/9q8d9u.png">
+</div>
+</div>
 </div>
 
 <div id="wrapper" style="width:100%;">
