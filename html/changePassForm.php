546d2cb38d1eed2766832f6b26b5f2c6200f6613
Kyle Sutherland <kysuther@live.ca>
2015-11-30 20:25:10 -0500

    renamed non php extention files

---
+++ html/changePassForm.php
@@ -0,0 +1,31 @@
+<?php include("header.php") ?>
+
+<form style="z-index:9999999" action="changePass.php" method="post">
+<br/>
+<br/>
+
+<div >
+    <p>
+    	<label for="emailAddress">Email Address:</label>
+        <input type="text" name="email" id="emailAddress">
+    </p>
+
+	<p>
+    	<label for="password">Current Password:</label>
+        <input type="password" name="password" id="password">
+    </p>
+    <p>
+    	<label for="password">New Password:</label>
+        <input type="password" name="password" id="password">
+    </p>
+    <p>
+    	<label for="password">Confirm New Password:</label>
+        <input type="password" name="password" id="password">
+    </p>
+
+
+    <input type="submit" value="REGISTER">
+    </div>
+</form>
+
+<?php include("footer.php") ?>
