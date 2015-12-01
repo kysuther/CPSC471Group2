a11616c7fa1fa88410225aa7bc37910d0362f6e8
Andrew Dong <andrew.mh.dong@gmail.com>
2015-11-30 17:03:02 -0700

    JOSH please gib hand and format

--- html/artistSearch.php
+++ html/artistSearch.php
@@ -1,18 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Search for Artist</title>
-</head>
-<body>
-<form action="searchArtist.php" method="post">
-	<p>
-    	<label for="Artist/Band">Enter Name of Artist/Band</label>
-        <input type="text" name="Artist/Band" id="Artist/Band">
-    </p>
-
+<?php
 	
-    <input type="submit" value="Search">
-</form>
-</body>
-</html>
+	$link  = mysqli_connect("localhost", "root", "", "CPSC471_musicdatabase");
+
+	if($link === false)
+	{
+		die("ERROR: could not connect. " . mysqli_connect_error());
+	}
+	
+	$Artist_name = mysqli_real_escape_string($link, $_POST['Artist/Band']);
+	
+	//echo "$Artist_name";
+	
+	$sql = "SELECT A.name,S.songName, S.length,S.genre
+			FROM Artists AS A, song AS S
+			WHERE A.name = '$Artist_name' AND A.ArtistID = S.AID ";
+	
+	if($result = mysqli_query($link, $sql))
+	{
+		if(mysqli_num_rows($result) > 0)
+		{
+			echo "<table>";
+				echo "Results containing $Artist_name";
+				echo "<tr>";
+					//echo"<th>ArtistID</th>";
+					echo"<th>ARTIST NAME:</th>";
+					//echo"<th>webpage</th>";
+					echo"<th>\tList of Songs:</th>";
+					echo"<th>\tSong Length:</th>";
+					echo"\t";
+					echo"<th>Genre:</th>";
+					
+					
+				echo"</tr>";
+			while($row = mysqli_fetch_array($result))
+			{
+				echo "<tr>";
+					echo "<td>" . $row['name']. "</td>";
+					echo "<td>" . $row['songName']. "</td>";
+					echo "<td>" . $row['length']. "</td>";
+					echo "<td>" . $row['genre']. "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+			echo "\n";
+			
+			mysqli_free_result($result);
+		}
+		else
+		{
+			echo "No matching results";
+		}
+		
+	}
+
+	else
+	{
+		echo "ERROR: Could not execute $sql." . mysqli_error($link);
+	}
+	
+	
+	
+	mysqli_close($link);
+
+?>
