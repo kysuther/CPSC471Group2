2b6290da4e441c909808f194240a17a5f1895ec8
Charlie Cheung <charliecheung@users.noreply.github.com>
2015-12-01 14:15:56 -0700

    removed duplicate code

--- html/register.php
+++ html/register.php
@@ -1,203 +1,11 @@
 <!--REGISTER PAGE-->
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-<meta name="p:domain_verify" content="3be8d8c7e105bf73047bb30813cab21e"/>
-<link href='http://fonts.googleapis.com/css?family=Playfair+Display' rel='stylesheet' type='text/css'>
 
-<link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript"  src='http://cdn.renderedfont.com/js/renderedfont-0.8.min.js#free'></script>
+<?php include 'headerphp.php' ?>
 
-<title>Register</title>
-{block:Description} <meta name="description" content="{MetaDescription}"
-/>{/block:Description}
-<link rel="shortcut icon" href="{Favicon}" />
 
-<meta charset="UTF-8">
-<title>Register</title>
-<style type="text/css">
-body{
-	margin: 0px;
-	padding: 0px;
-	background:#fff url('http://i65.tinypic.com/2lc8so5.png') fixed;
-}
-::-webkit-scrollbar {
-    width: 9px;
-    height: 3px;
-    background: #fff;
-}
-::-webkit-scrollbar-thumb {
-    background-color:#d4d4d4;
-    border-radius: 5px solid black;
-}
-#topbar {
-    position:fixed;
-    z-index:99999999;
-    width:100%;
-    height:50px;
-    left:0px;
-    top:0px;
-    background:#525252;
-}
-#title a {
-	position:fixed;
-	margin-top:5px;
-	margin-left:20px;
-	letter-spacing:2px;
-	text-decoration:none;
-	font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-	color:#fff;
-	font-size:20px;
-	-webkit-transition: 0.5s ease;
-	transition: 0.5s ease;
-    -moz-transition: 0.5s ease;
-    -o-transition: 0.5s ease;
-}
-#title a:hover {
-	position:fixed;
-	font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-	text-decoration:none;
-	color:#d4d4d4;
-}
-#topimage {
-    position:absolute;
-    top:20px;
-}
-#topimage img {
-    height:100%;
-    width:100%;
-}
-#subtitle {
-    position:fixed;
-    letter-spacing:1px;
-    text-decoration:none;
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color:#63d297;
-    margin-top:28px;
-    margin-left:20px;
-}
-#userlinks {
-    position:fixed;
-    z-index:999999999;
-    margin-top:27px;
-    right:-1px;
-}
-#userlinks a {
-    text-decoration:none;
-    text-transform:none;
-    float:left;
-    margin-right:15px;
-    letter-spacing:1px;
-    padding:10px 20px 12px 17px;
-    font-size:16px;
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color:#fff;
-    background:#63d297;
-    -webkit-transition: 0.8s ease;
-    transition: 0.8s ease;
-    -moz-transition: 0.8s ease;
-    -o-transition: 0.8s ease;
-    border:1px solid #63d297;
-}
-#userlinks a:hover {
-    text-transform:none;
-    text-decoration:none;
-    color:#63d297;
-    background:#fff;
-    border:1px solid #525252;
-}
-form {
-    position:fixed;
-    z-index:9999999;
-    top:250px;
-    left:100px;
-    width: 350px;
-    font-style:none;
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; 
-    color:#fff;
-    letter-spacing:1px;
-    font-size:20px;
-    background-color:#2a2a2a;
-    padding:10px 10px 10px 10px;
-    border:1px solid #2a2a2a;
-    border-radius: 20px;
-}
-button {
-    width:120px;
-    height:45px;
-    margin-left:32%;
-    font-style:none;
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; 
-    color:#fff;
-    letter-spacing:1px;
-    font-size:20px;
-    background-color:#525252;
-}
-button:hover {
-    background-color:#63d297;
-}
-#tabbox {
-text-align: left;
-font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-font-size: 14px; /*self-explanatory*/
-position:fixed;
-left:0px;
-top:-120px;
--webkit-transition: all .6s ease-in-out;
--moz-transition: all .6s ease-in-out;
--o-transition: all .6s ease-in-out;
-z-index:99999;
-}
- 
-#tabbox:hover {top:0px;}
- 
-#boxhover {
-padding: 9px;
-background: #525252; /*Color of the tab box background*/
-width:355px;
-color:#ffffff; /*color of the box text*/
-height:150px; /*how tall it is*/
-z-index: 9999;
-}
- 
-#tablabel{
-font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-font-size:14px;
-letter-spacing:1px;
-text-align: center;
-color:#ffffff; /*Color of the tab title*/
-height:20px;
-padding: 7px 0px 3px 0px;
-background:#525252; /*Color of the tab label background*/
-width: 105px;
-}
 </style>
 </head>
 <body>
-<div id="topbar">
-<div id="title"><a href="/"><b>CPSC471 Group02</b></a></div>
-<div id="subtitle">Fall 2015 | University of Calgary</div>
-    <div id="userlinks">
-	<a href="/"><b>log in</b></a>
-	<a href="/"><b>register</b></a>
-</div>
-</div>
-
-<div id="tabbox">
-<div id="boxhover"><p>fillertext</p>
-<p>
-<font color="#fff">Andrew Dong, Charlie Cheung, Joshua Hong, Kyle Sutherland</br>CPSC471 Group02</br>Prof: Nelson Wong</br>TA: Ibrahim Karakira
-</p>
-</div> <!--TABBOX END-->
-<div id="tablabel">Student Info</div>
-</div>
-
-<div id="topimage">
-    <img src="http://i64.tinypic.com/2nrd9cn.jpg">
-</div>
 
 <form action="createAccountAdd.php" method="post">
 	<p>
@@ -222,19 +30,6 @@ width: 105px;
     <button type="submit" value="REGISTER">Register</button>
 </form>
 
-<p><span title="back to top"><a href="#top" style="position: fixed; bottom: 10px; right: 10px; text-decoration: none; color: #63d297; font-size: 20px; z-index: 9999999999;"><img src="http://i67.tinypic.com/2roma03.png"></a></span></p> 
-
-
-<div id="thefooter">
-    <div id="footertitle"><b>CPSC471 Group02</b></div>
-    <div id="footernote">University of Calgary | Fall2015 | Dr. Nelson Wong | TA: Ibrahim Karakira</br>
-    <i>Andrew Dong, Charlie Cheung, Joshua Hong, Kyle Sutherland</br>**This website is created for the purpose of completing the CPSC71 term project for Fall 2015 only.</br> The content on this webpage is not made for profit. </br>We claim no ownership for the photos and songs on this webpage.</i></div>
-</div>
-
-
-
-
-
 
 
 </div>
@@ -242,3 +37,5 @@ width: 105px;
 
 </body>
 </html>
+
+<?php include 'footerphp.php' ?>
