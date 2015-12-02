7ebd144be3a45f09df654aaf86cdb7a23411c9f1
Andrew Dong <andrew.mh.dong@gmail.com>
2015-12-02 09:23:43 -0700

    Stupid Mistakes

--- html/SearchForm.php
+++ html/SearchForm.php
@@ -5,7 +5,7 @@
 <title>Search for Artist</title>
 </head>
 <body>
-<form action="artistSearch.php" method="post">
+<form action="Search.php" method="post">
 	<p>
     	<label for="SearchField">Enter Name of Artist/Band or Album or Songs</label>
         <input type="text" name="SearchField" id="SearchField">
