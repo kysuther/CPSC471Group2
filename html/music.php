bf52455cfc99ac22bd91927be82dc09618a4dc71
Kyle Sutherland <kysuther@live.ca>
2015-12-02 22:10:14 -0500

    fixed music.php error

--- html/music.php
+++ html/music.php
@@ -17,7 +17,7 @@
             while(($sRow = mysqli_fetch_array($sresult))&&($aRow = mysqli_fetch_array($aresult)))
 			{
 				echo "<tr>";
-					echo '<td style="text-align:center;font-size:110%; font-family:'Helvetica', Helvetica, Arial, sans-serif;">' . $sRow['songName']. "</td>";
+					echo '<td style="text-align:center;font-size:110%; font-family:Helvetica, Helvetica, Arial, sans-serif;">' . $sRow['songName']. "</td>";
                     echo '<td style="text-align:center;font-size:110%;">' . $aRow['name']. "</td>";
 				echo "</tr>";
             }
