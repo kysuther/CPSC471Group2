e596018bad5228a3f6f61dd50b45493f741d8468
Kyle Sutherland <kysuther@live.ca>
2015-12-03 02:57:21 -0500

    changes to admin page

--- html/adminPage.php
+++ html/adminPage.php
@@ -3,20 +3,20 @@
 	include 'requireslogin.php';
 	include 'header.php';
 ?>
-<h1>Administration Page</h1>
+<h1 align='center'>Administration Page</h1>
 <?php 
 	$sql = 'SELECT * FROM user';
 	$result = mysqli_query($link, $sql);
 	if($result){
 		if(mysqli_num_rows($result) > 0){
-			echo "<table cellpadding='10' border='4' style='width:50%;background-color:White;'>";
+			echo "<table align='center' cellpadding='10' border='4' style='width:50%;background-color:White;'>";
 			echo "<tr>";
 			echo'<th style="font-size:100%">User ID</th>';
 			echo'<th style="font-size:100%">Email Address</th>';
 			echo'<th style="font-size:100%">First Name</th>';
 			echo'<th style="font-size:100%">Last Name</th>';
 			echo'<th style="font-size:100%">Access Level</th>';
-			echo'<th style="font-size:100%">Edit User</th>';
+			echo'<th style="font-size:100%">Delete User</th>';
 			echo"</tr>";
 			
 			while($row = mysqli_fetch_array($result)){
@@ -26,7 +26,11 @@
 				echo '<td style="text-align:center;font-size:100%;">' . $row['Fname']. "</td>";
 				echo '<td style="text-align:center;font-size:100%;">' . $row['Lname']. "</td>";
 				echo '<td style="text-align:center;font-size:100%;">' . $row['Type']. "</td>";
-				echo '<td style="text-align:center;font-size:100%;"><div id="userbuttons"><a href="/"><b>Edit</b></a></div></td>';
+				if($row['Type'] != 'admin'){
+					echo '<td style="text-align:center;font-size:100%;"><div id="userbuttons"><a href="/"><b>Delete</b></a></div></td>';
+				}else{
+					echo '<td style="text-align:center;font-size:100%;">Not Allowed</td>';
+				}
 				echo '</tr>';
 			}
 			
