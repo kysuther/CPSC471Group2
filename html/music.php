0f17c4cc0da7f91a13767d408d2ee9e56c2808cb
Charlie Cheung <charliecheung@users.noreply.github.com>
2015-12-02 19:32:54 -0700

    style changes pls

--- html/music.php
+++ html/music.php
@@ -9,7 +9,7 @@
     {
         if((mysqli_num_rows($sresult) > 0) && (mysqli_num_rows($aresult) > 0))
 		{
-            echo "<table cellpadding='10' border='4' style='width:50%;background-color:LimeGreen;'>";
+            echo "<table cellpadding='10' border='4' margin:auto; top:0; bottom:0; left:0; right:0; style='width:50%;'>";
                 echo "<tr>";
                     echo'<th style="font-size:200%">Track</th>';
                     echo'<th style="font-size:200%">Artist</th>';
@@ -17,7 +17,7 @@
             while(($sRow = mysqli_fetch_array($sresult))&&($aRow = mysqli_fetch_array($aresult)))
 			{
 				echo "<tr>";
-					echo '<td style="text-align:center;font-size:110%;">' . $sRow['songName']. "</td>";
+					echo '<td style="text-align:center;font-size:110%; font-family:'Helvetica', Helvetica, Arial, sans-serif;">' . $sRow['songName']. "</td>";
                     echo '<td style="text-align:center;font-size:110%;">' . $aRow['name']. "</td>";
 				echo "</tr>";
             }
