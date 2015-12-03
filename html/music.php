12c84a1ce55297606149972b06c5b1e2478ddc8d
Andrew <andrew.mh.dong@gmail.com>
2015-12-03 00:06:36 -0700

    pushing the music.php

--- html/music.php
+++ html/music.php
@@ -36,5 +36,24 @@
     {
         echo "ERROR: Could not execute $sql." . mysqli_error($link);
     }
+	
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8">
+	<title>Search for Artist</title>
+	</head>
+	<body>
+	<form action="searchData.php" method="post">
+	<p>
+		<label for="SearchField">Enter Name of Artist/Band or Album or Songs</label>
+		<input type="text" name="SearchField" id="SearchField">
+	</p>
+
+	<input type="submit" value="Search">
+	</form>
+	</body>
+	</html>
+	
+	
 (include "footer.php");
 ?>
